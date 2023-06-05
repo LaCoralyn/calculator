c6b3ece05628b6647ca3dc0e821cc8ecc7952598
LaCoralyn Hilliard <lacoralyn.hilliard@gmail.com>
2023-06-05 15:36:50 -0400

    First Day of Calculator Project
    Create calculator operation functions, boilerplate
     and basic css.

---
+++ calculator.js
@@ -0,0 +1,26 @@
+
+let a;//first numner
+let b;//second number
+let x;// operator
+function add(a,b){
+    let sum = a+b;
+    console.log( sum);
+};
+
+function subtract(a,b){
+    let sum= a-b;
+    console.log( sum);
+};
+
+function multi(a,b){
+    let fin = a*b;
+    console.log(fin);
+};
+function div(a,b){
+    let fin = a/b;
+    console.log( fin);
+};
+function operate(a,b,x){
+    add(a,b);
+};
+
