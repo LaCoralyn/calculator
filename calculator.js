8e4c936f85453ff909c5fd9f187f5b03927f338d
LaCoralyn Hilliard <lacoralyn.hilliard@gmail.com>
2023-06-07 16:47:02 -0400

    Create Supporting functions -created objects and reworked html and css

--- calculator.js
+++ calculator.js
@@ -1,7 +1,34 @@
+const numbers = document.querySelectorAll('[data-number]');
+const operationBttns= document.querySelectorAll('[data-operation]');
+const equalsBttn= document.querySelector('[data-equals]')
+const deleteBttn= document.querySelector('[data-delete');
+const allClearBttn = document.querySelector('[data-all-clear]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText= document.querySelector('[data-current-operand]');
+class Calculator {
+    constructor(previousOperandText, currentOperandText) {
+      this.previousOperandText = previousOperandText
+      this.currentOperandText = currentOperandText
+      clear();
+    
+    };
+   
+  };
+  const calculator = new Calculator(previousOperandText, currentOperandText);
+  numbers.forEach(button => {
+    button.addEventListener('click', () => {
+      Calculator.appendNumber(button.innerHTML)
+      Calculator.updateDisplay();
+    });
+});
+  console.log(appendNumber());
 
-let a;//first numner
-let b;//second number
-let x;// operator
+
+  function clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined; };
+
 function add(a,b){
     let sum = a+b;
     console.log( sum);
@@ -22,5 +49,17 @@ function div(a,b){
 };
 function operate(a,b,x){
     add(a,b);
+};
+
+
+function appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+};
+
+
+
+function updateDisplay() {
+    this.currentOperandText.innerText = this.currentOperand;
 };
 
